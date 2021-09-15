dd4f0beac41a627c470f917b5d41db911401a975
Nane2501 <90798618+Nane2501@users.noreply.github.com>
2021-09-16 02:56:25 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "hello, yêu Na không?",
   descriptionHeader:
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  buttonNo: "đéo.",
+  titleModar: "zeeee zê. làm người yêu mình nhaaaaa",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'đùa thôi, mình làm gì đã có cr </br> lo học đã không mẹ đánh </br> "mà trung thu này đi nhởi không, chơ chưa có ai rủ đi cả, buồn tê </br>  hehee.  "',
 };
